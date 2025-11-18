963725419460c8bf83f49f746a1fe2ffcd03cd3b
NCV17 <nguyencongvuk17@gmail.com>
2025-11-18 21:58:50 +0700

    fix giao dien

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace CodeForge_Desktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainFormStudent());
+            Application.Run(new MainFormAdmin());
         }
     }
 }
