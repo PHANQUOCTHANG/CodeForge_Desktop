ddb290d24ef57e4c89e18cd0db9ad16c3a5676b6
NTann17 <tantrinh209@gmail.com>
2025-11-19 11:45:13 +0700

    Up FE: StudentSettings

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace CodeForge_Desktop
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainFormAdmin());
+            Application.Run(new MainFormStudent());
         }
     }
 }
